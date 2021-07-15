4a2fc652ae40c697183baf7560fe9aa8a47efe03
Teerachai Laothong <tlaothong@gmail.com>
2021-07-15 17:44:42 +0700

    Added a Demo web api

---
+++ TheDemo/DemoWebApi/Controllers/DemoController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DemoWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public Post Get(int id)
+        {
+            return new Post
+            {
+                Data = new Data
+                {
+                    Id = id,
+                    Title = $"Title #{id} {new string('*', id % 8)}",
+                },
+            };
+        }
+    }
+
+    public partial class Post
+    {
+        [JsonPropertyName("meta")]
+        public object Meta { get; set; }
+
+        [JsonPropertyName("data")]
+        public Data Data { get; set; }
+    }
+
+    public partial class Data
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }
+
+        [JsonPropertyName("user_id")]
+        public long UserId { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("body")]
+        public string Body { get; set; }
+    }
+
+}
